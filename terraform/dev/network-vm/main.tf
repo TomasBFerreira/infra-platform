84bde3d3af809d33f112dcdc91915caed14ef900
Tomas B Ferreira <all@tomasbrazao.com>
2025-11-05 09:18:30 +0000

    Add Proxmox VM configuration for network VM

--- terraform/dev/network-vm/main.tf
+++ terraform/dev/network-vm/main.tf
@@ -55,3 +55,37 @@ variable "proxmox_password" {
 data "vault_generic_secret" "ssh_key" {
   path = "secret/ssh_keys/media-stack_worker"
 }
+
+resource "proxmox_vm_qemu" "network_vm" {
+  vmid        = 220
+  hostname    = "network-vm"
+  ostemplate  = "local:vztmpl/debian-12-standard_12.7-1_amd64.tar.zst"
+  cores       = 2
+  memory      = 2048
+  
+  rootfs {
+    storage = "local-lvm"
+    size    = "25G"
+  }
+  
+  network {
+    name   = "eth0"
+    bridge = "vmbr0"
+    ip     = "192.168.50.220/24"
+    gw     = "192.168.50.1"
+  }
+  
+  unprivileged = true
+  
+  features {
+    nesting = true
+  }
+  
+  ssh_public_keys = data.vault_generic_secret.ssh_key.data["public"]
+  start           = true
+  target_node     = "betsy"
+}
+
+output "network_vm_ip" {
+  value = "192.168.50.220"
+}
