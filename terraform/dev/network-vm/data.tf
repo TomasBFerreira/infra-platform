17752fdced9790ed3457752a96282f668f97a9ec
TomasBFerreira <all@tomasbrazao.com>
2025-11-09 16:37:48 +0000

    removed need for vault for now

--- terraform/dev/network-vm/data.tf
+++ terraform/dev/network-vm/data.tf
@@ -1,3 +1,1 @@
-data "vault_generic_secret" "ssh_key" {
-  path = "secret/ssh_keys/media-stack_worker"
-}
+# No data sources needed for now
--- terraform/dev/network-vm/providers.tf
+++ terraform/dev/network-vm/providers.tf
@@ -6,10 +6,6 @@ terraform {
       source  = "telmate/proxmox"
       version = "~> 2.9"
     }
-    vault = {
-      source  = "hashicorp/vault"
-      version = "~> 3.0"
-    }
   }
 
   backend "local" {
@@ -18,9 +14,9 @@ terraform {
 }
 
 provider "proxmox" {
-  pm_api_url          = var.proxmox_api_url
-  pm_user             = var.proxmox_user
-  pm_password         = var.proxmox_password
+  pm_api_url          = var.pve_api
+  pm_user             = var.pve_user
+  pm_password         = var.pve_pass
   pm_tls_insecure     = true
   pm_log_enable       = true
   pm_log_file         = "terraform-plugin-proxmox.log"
@@ -29,9 +25,4 @@ provider "proxmox" {
     _default    = "debug"
     _capturelog = ""
   }
-}
-
-provider "vault" {
-  address = var.vault_address
-  token   = var.vault_token
 }
