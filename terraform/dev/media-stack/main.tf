f9586c4ca14c88fda4ee0f42dc4bc71c0a2b412c
root <root@l-ct-dev1.databaes.net>
2026-01-04 01:38:32 +0000

    fixed media-stack proxmox api usage and chmoded script for ssh key gen

--- terraform/dev/media-stack/main.tf
+++ terraform/dev/media-stack/main.tf
@@ -17,10 +17,10 @@ terraform {
 }
 
 provider "proxmox" {
-  pm_api_url      = var.proxmox_api_url
-  pm_user         = var.proxmox_user
-  pm_password     = var.proxmox_password
-  pm_tls_insecure = true
+  pm_api_url          = var.proxmox_api_url
+  pm_api_token_id     = var.proxmox_user
+  pm_api_token_secret = var.proxmox_password
+  pm_tls_insecure     = true
 }
 
 provider "vault" {
