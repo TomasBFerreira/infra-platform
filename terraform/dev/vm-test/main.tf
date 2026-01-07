deac30eb257bbf630640678af2ad21ca00ea8955
tomasbferreira <all@tomasbrazao.com>
2026-01-07 18:59:15 +0000

    Add test VM deployment workflow and minimal config

--- terraform/dev/vm-test/main.tf
+++ terraform/dev/vm-test/main.tf
@@ -1,33 +1,27 @@
 module "test_vm" {
   source = "../../modules/proxmox-vm"
 
-  name        = "test-vm"
-  description = "Test VM for workflow validation"
+  # Minimal config for testing
+  name        = "test-vm-minimal"
+  description = "Minimal test VM"
   vmid        = 300
   target_node = "benedict"
 
-  cores           = 1
-  sockets         = 1
-  memory          = 1024
-  qemu_agent_enabled = true
+  # Hardware - minimal
+  cores   = 1
+  memory  = 512
 
-  clone_template    = "ubuntu-24.04-cloudinit-template"
-  boot_order        = "order=virtio0;ide2"
-  bios              = "seabios"
-  scsihw            = "virtio-scsi-pci"
+  # Clone from template
+  clone_template = "ubuntu-24.04-cloudinit-template"
+  bios           = "ovmf"
 
-  network_model   = "virtio"
-  network_bridge  = "vmbr0"
+  # Network - basic
   network_ip      = "192.168.50.300/24"
   network_gateway = "192.168.50.1"
 
-  disk_type    = "virtio"
-  disk_storage = "shared-ssd-nfs"
-  disk_size    = "8G"
-  disk_format  = "qcow2"
-  disk_ssd     = 1
-  disk_discard = "on"
+  # Disk - minimal
+  disk_size = "8G"
 
-  cloudinit_enabled  = true
-  cloudinit_user     = "ubuntu"
+  # Cloud-init
+  cloudinit_enabled = true
 }
