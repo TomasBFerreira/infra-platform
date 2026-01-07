6d05a4d84ff3f2015f0aedb5802ac3c11a1f3a17
tomasbferreira <all@tomasbrazao.com>
2026-01-07 19:21:11 +0000

    Update vm-test to match template boot order and disk type

--- terraform/dev/vm-test/main.tf
+++ terraform/dev/vm-test/main.tf
@@ -15,13 +15,16 @@ module "test_vm" {
   # Clone from template
   clone_template = "ubuntu-24.04-cloudinit-template"
   bios           = "ovmf"
+  boot_order     = "order=scsi0;net0"
 
   # Network - basic
   network_ip      = "192.168.50.300/24"
   network_gateway = "192.168.50.1"
 
-  # Disk - minimal
-  disk_size = "8G"
+  # Disk - minimal, match template
+  disk_type   = "scsi"
+  disk_size   = "8G"
+  disk_format = "qcow2"
 
   # Cloud-init
   cloudinit_enabled = true
