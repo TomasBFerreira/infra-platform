382b19570fe05f1202d553ab7aaf844953407140
tomasbferreira <all@tomasbrazao.com>
2026-01-07 19:12:29 +0000

    Align vm-test provider and module config with network-vm (add sockets, logging/debug)

--- terraform/dev/vm-test/providers.tf
+++ terraform/dev/vm-test/providers.tf
@@ -18,6 +18,13 @@ provider "proxmox" {
   pm_api_token_id     = var.pve_user
   pm_api_token_secret = var.pve_pass
   pm_tls_insecure     = true
+  pm_log_enable       = true
+  pm_log_file         = "terraform-plugin-proxmox.log"
+  pm_debug            = true
+  pm_log_levels = {
+    _default    = "debug"
+    _capturelog = ""
+  }
 }
 
 provider "vault" {
