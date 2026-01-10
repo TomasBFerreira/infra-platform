d919896f37079c05ac9dbaca690e96a30021c079
TomasBFerreira <all@tomasbrazao.com>
2026-01-10 17:53:55 +0000

    Fix Proxmox provider configuration for Vault LXC Terraform

---
+++ terraform/prod/vault-lxc/providers.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    proxmox = {
+      source  = "bpg/proxmox"
+      version = "~> 0.39.0"
+    }
+  }
+}
+
+provider "proxmox" {
+  endpoint = var.proxmox_api_url
+  username = var.proxmox_user
+  password = var.proxmox_password
+  insecure = true
+  timeout  = 600
+}
--- terraform/prod/vault-lxc/variables.tf
+++ terraform/prod/vault-lxc/variables.tf
@@ -1,6 +1,45 @@
-variable "lxc_template" {}
-variable "target_node" {}
-variable "lxc_id" {}
-variable "lxc_ip" {}
-variable "lxc_gw" {}
-variable "ssh_public_keys" {}
+variable "proxmox_api_url" {
+  description = "Proxmox API URL"
+  type        = string
+}
+
+variable "proxmox_user" {
+  description = "Proxmox username"
+  type        = string
+}
+
+variable "proxmox_password" {
+  description = "Proxmox password"
+  type        = string
+  sensitive   = true
+}
+
+variable "lxc_template" {
+  description = "LXC template name/path"
+  type        = string
+}
+
+variable "target_node" {
+  description = "Proxmox node/host where LXC will be created"
+  type        = string
+}
+
+variable "lxc_id" {
+  description = "LXC VM ID"
+  type        = number
+}
+
+variable "lxc_ip" {
+  description = "LXC IP address with CIDR"
+  type        = string
+}
+
+variable "lxc_gw" {
+  description = "Gateway IP for LXC"
+  type        = string
+}
+
+variable "ssh_public_keys" {
+  description = "SSH public keys for LXC"
+  type        = string
+}
